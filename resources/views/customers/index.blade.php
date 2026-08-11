bb360e04abfd56f2c77fd8b345a203f66ba78005
Nordin GoforIT <n.boufouchk@goforit.nl>
2026-08-11 14:44:34 +0200

    Replace textarea with input for address field in customer form for improved usability

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -231,8 +231,8 @@
                             <!-- Adres -->
                             <div>
                                 <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Adres</label>
-                                <textarea name="address" rows="2" :value="currentCustomer?.address"
-                                          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white"></textarea>
+                                <input type="text" name="address" :value="currentCustomer?.address"
+                                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                             </div>
 
                             <!-- Postcode, Plaats, Land -->
